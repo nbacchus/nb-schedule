2837ba01bc424d72db3563e29796f660ed6c8eb9
Nadia Bacchus <nadiabacchus@Nadias-MacBook-Pro.local>
2022-07-14 18:12:57 -0400

    updated js functions

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,4 +1,39 @@
 //link current time and day with moment.js
 var currentTime = moment().format("MMM Do YY");
 $("#currentDay").text(currentTime)
+var currentHour = moment().format("HH");
 
+//checks current time to change color
+$(".timeReader").each(function () {
+var timeReader = $(this).attr("id").split("-")[1];
+    if (currentHour == timeReader) {
+      $(this).addClass("present");
+      $(this).children("description").addClass("white-text");
+    } else if (currentHour < timeReader) {
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    } else if (currentHour > timeReader) {
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    }
+});
+
+//click on description to change
+$(".col-9").on("click", function() {
+    var text = $(this)
+       .text()
+       .trim();
+   
+     var textInput = $("<textarea>").addClass("col-9").val(text);
+     $(this).replaceWith(textInput);
+
+     textInput.trigger("focus");
+   });
+
+   //save in local storage
+   $(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var value = $(this).siblings(".time-block").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, value);
+  });
